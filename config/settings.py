d1b981444fde1b132e9941b9c8cd0d3b64c473b1
srinath001ch <srinathnetha001285@example.com>
2026-06-30 12:56:14 +0530

    fix aloow user

--- config/settings.py
+++ config/settings.py
@@ -18,9 +18,8 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
+    "127.0.0.1,localhost,trio-self-drive.onrender.com"
 ).split(",")
-
 
 # Application definition
 
