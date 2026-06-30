78dcb64ea9d270259fa16768e9ce12f2453bad74
srinath001ch <srinathnetha001285@example.com>
2026-06-30 22:49:56 +0530

    Fix Django 5 Cloudinary storage

--- config/settings.py
+++ config/settings.py
@@ -164,4 +164,11 @@ cloudinary.config(
     api_secret=os.environ.get("PRNtDHha-8OzKhrb7f4gZbNtkUo"),
 )
 
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
